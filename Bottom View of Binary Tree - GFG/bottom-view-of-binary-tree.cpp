fd880aeec3781646ee938f2505eb0d9d69aa61dc
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-06-26 22:14:18 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
@@ -103,8 +103,6 @@ class Solution {
         queue<pair<Node*,int>>q;
         q.push({root,0});
         while(!q.empty()){
-            int n = q.size();
-            for(int i=0; i<n; i++){
                 Node *temp = q.front().first;
                 int dis = q.front().second;
                 q.pop();
@@ -113,7 +111,6 @@ class Solution {
                     q.push({temp->left,dis-1});
                 if(temp->right!=NULL)
                     q.push({temp->right,dis+1});
-            }
         }
         for(auto x:mp)
             ans.push_back(x.second);
